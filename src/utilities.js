2443c0e2ee9c13e41c4fc750ea9c906cc7cacd31
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2020-07-18 22:05:12 +0200

    feat: add skill generator

--- src/utilities.js
+++ src/utilities.js
@@ -6,3 +6,8 @@ const weapon = ['stripe', 'paws', 'fang', 'claw', 'tail', 'purr', 'smell', 'cudd
 const shuffle = (array) => Math.floor(Math.random() * array.length);
 
 export const createRandomName = () => `${shuffle(cat)} ${shuffle(skill)}${shuffle(weapon)}`;
+
+const skillName = ['Smell', 'Purr', 'Meow', 'Leap', 'Bite', 'Scratch', 'Snore', 'Sleep', 'Furball']
+const skillDescription = ['Doom', 'Destruction', 'Vegance', 'Destiny', 'Freedom', 'Glory', 'Purrification', 'Tail', 'Stripes']
+
+export const createRandomSkill = () => `${shuffle(skillName)} of ${shuffle(skillDescription)}`;
